58ecff9d4ac564619131fa31a7008e3a6e15d85c
DemonLemon5 <126193596+DemonLemon5@users.noreply.github.com>
2023-04-05 11:22:12 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,7 @@
+
+public class Test extends RuntimeException {
+        public Test(String message) {
+            super(message);
+        }
+    }
+
---
+++ Tester.java
@@ -0,0 +1,22 @@
+public class Tester extends Character {
+    public static final String requiredTesterName = "Test";
+
+    public Tester(String name) {
+        super(name, 10, 10, 10);
+        if (!name.equals(requiredTesterName)) {
+            throw new Test("Invalid tester name.");
+        }
+    }
+
+    @Override
+    public void sayHello() {
+        System.out.println("Hello, I am a tester named " + getName() + ".");
+    }
+
+    @Override
+    public int calculatePower() {
+        return 0;
+    }
+}
+
+
